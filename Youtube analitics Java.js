8c741349a48079387c3ba7930f87129fa2db4a5e
MesicTim15 <46650644+MesicTim15@users.noreply.github.com>
2021-07-18 11:50:10 +0200

    Add files via upload

---
+++ Youtube analitics Java.js
@@ -0,0 +1,12 @@
+use Databox\Client;
+
+$c = new Client('w22ydkwnafn29k5w74881d');
+
+$c->insertAll([
+    ['Likes', 0, '2021-07-11'],
+    ['Views_by_video', 13, '2021-06-16'],
+    ['Watch_time_by_traffic_source', 2, '2021-06-16],
+    ['Comments' 0, '2021-06-16],
+    ['Views' 15, '2021-06-16],
+    ['Video_watch_time_by_traffic_source' 540, '2021-06-16],
+]);
